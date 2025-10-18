d1a2f952e360b8ac97eb08e048f2ab8fbadf6c4a
comfysquirrel <jdh11235@gmail.com>
2025-10-17 23:45:37 -0500

    Sort blocks by color and print it

--- test.py
+++ test.py
@@ -3,13 +3,24 @@ from PIL import Image, ImageStat
 
 input_folder = 'input'
 
-def print_average_hue(filename):
+output_data = []
+
+def get_average_hue(filename):
     img = Image.open(filename).convert('RGB').convert('HSV')
     h, s, v = img.split()
-    average_hue = ImageStat.Stat(h).mean[0]
-
-    print(filename, average_hue)
+    return ImageStat.Stat(h).mean[0]
 
 for filename in os.listdir(input_folder):
     path = os.path.join(input_folder, filename)
-    print_average_hue(path)
+    hue = get_average_hue(path)
+    output_data.append({
+        'filename': filename,
+        'hue': hue
+    })
+
+def sorter(list):
+    return list['hue']
+
+output_data.sort(key=sorter)
+
+print(output_data)
