c9c1690511a9cb244c111d3a54182c7688a82eb6
comfysquirrel <jdh11235@gmail.com>
2025-10-18 01:19:49 -0500

    Make square composite image sorted by hue

--- test.py
+++ test.py
@@ -6,29 +6,33 @@ input_folder = 'input'
 output_folder = 'output'
 texture_size = 16
 
-number_of_textures = len(os.listdir(input_folder))
-background_size = math.ceil(math.sqrt(number_of_textures)) * texture_size
-composite_image = Image.new("RGBA", (background_size, background_size), (0, 0, 0, 0))
+number_of_blocks = len(os.listdir(input_folder))
+background_width_blocks = math.ceil(math.sqrt(number_of_blocks))
+background_width_pixels = background_width_blocks * texture_size
+composite_image = Image.new("RGBA", (background_width_pixels, background_width_pixels), (0, 0, 0, 0))
 
-textures = []
+blocks = []
 
 for filename in os.listdir(input_folder):
     path = os.path.join(input_folder, filename)
     img = Image.open(path).convert('RGBA')
     h, s, v = img.convert('HSV').split()
     average_hue = ImageStat.Stat(h).mean[0]
-    texture_name = filename.split('.')[0]
-    textures.append({
-        'name': texture_name,
+    block_name = filename.split('.')[0]
+    blocks.append({
+        'name': block_name,
         'hue': average_hue,
         'image': img
     })
 
-textures.sort(key=(lambda x: x['hue']))
+blocks.sort(key=(lambda x: x['hue']))
 
-for texture in textures:
-    composite_image.paste(texture['image'], (0, 0), texture['image'])
+for index, block in enumerate(blocks):
+    img = block['image']
+    col_blocks = index % background_width_blocks
+    row_blocks = math.floor(index / background_width_blocks)
+    col_pixels = col_blocks * texture_size
+    row_pixels = row_blocks * texture_size
+    composite_image.paste(img, (col_pixels, row_pixels), img)
 
 composite_image.save(f'{output_folder}/composite.png')
-
-# print(textures)
